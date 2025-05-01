26b16d8d0be1ffa2116da35b113996876d077a95
kevingr161 <kevindgutierrez161@gmail.com>
2025-04-30 22:50:19 -0600

    advances

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import { useRoutes } from "react-router-dom"
 import { Toaster } from "react-hot-toast"
+import routes from "./routes.jsx"
 
 
+ const App = () =>{
 
-export const App = () =>{
+  let element = useRoutes(routes);
+
   return(
     <>
-
+      {element}
+      <Toaster
+        position="bottom-right"
+        reverseOrder={false}
+      />
     </>
   )
 }
+export default App;
